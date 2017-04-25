61c501fd45caafbcad59647368b50dbd6a777962
francisco campos <franciscojavier.c.c@gmail.com>
2017-04-25 16:49:31 -0300

    Revert "Merge branch 'master' into master-develop"
    This reverts commit ac720b65edc1055faed393007f561bf843456a3e.
    
    # Conflicts:
    #	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
+=======
+  root 'home#index'
+  get 'home/index'
+>>>>>>> parent of ac720b6... Merge branch 'master' into master-develop
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
